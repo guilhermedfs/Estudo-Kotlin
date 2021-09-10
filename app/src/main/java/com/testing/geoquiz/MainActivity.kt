491246a200fd0d5c95d4592fadb8d47feb9b1271
Guilherme Daniel Fernandes da Silva <guilhermedfs11@gmail.com>
2021-09-09 21:05:39 -0300

    Added new Activity to app that show the answer of the question.

--- app/src/main/java/com/testing/geoquiz/MainActivity.kt
+++ app/src/main/java/com/testing/geoquiz/MainActivity.kt
@@ -1,22 +1,26 @@
 package com.testing.geoquiz
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 
 private const val TAG = "MainActivity"
+private const val KEY_INDEX = "index"
+private const val REQUEST_CODE_CHEAT = 0
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
     private lateinit var nextButton: Button
+    private lateinit var cheatButton: Button
     private lateinit var questionTextView: TextView
 
     private val quizViewModel: QuizViewModel by lazy {
@@ -28,18 +32,31 @@ class MainActivity : AppCompatActivity() {
         Log.d(TAG, "onCreate(Bundle?) called")
         setContentView(R.layout.activity_main)
 
+        val currentIndex = savedInstanceState?.getInt(KEY_INDEX, 0) ?: 0
+        quizViewModel.currentIndex = currentIndex
+
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
         nextButton = findViewById(R.id.next_button)
+        cheatButton = findViewById(R.id.cheat_button)
         questionTextView = findViewById(R.id.question_text_view)
 
 
-        trueButton.setOnClickListener{ view: View ->
+        trueButton.setOnClickListener{
             checkAnswer(true)
         }
 
-        falseButton.setOnClickListener{ view: View ->
+        falseButton.setOnClickListener{
             checkAnswer(false)
+        }
+
+        cheatButton.setOnClickListener{
+            val answerIsTrue = quizViewModel.currentQuestionAnswer
+            val intent = CheatActivity.newIntent(this@MainActivity, answerIsTrue)
+            startActivityForResult(
+                intent,
+                REQUEST_CODE_CHEAT
+            )
         }
 
         nextButton.setOnClickListener{
@@ -63,6 +80,12 @@ class MainActivity : AppCompatActivity() {
     override fun onPause(){
         super.onPause()
         Log.d(TAG, "onPause() called")
+    }
+
+    override fun onSaveInstanceState(savedInstanceState: Bundle) {
+        super.onSaveInstanceState(savedInstanceState)
+        Log.i(TAG, "onSaveInstaceState")
+        savedInstanceState.putInt(KEY_INDEX, quizViewModel.currentIndex)
     }
 
     override fun onStop(){
